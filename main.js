950efc1ca419969d38a4aea41f139d1332fceac7
skyelar <skyelarkessner@gmail.com>
2025-10-17 18:12:00 -0400

    main.js

--- main.js
+++ main.js
@@ -8,15 +8,25 @@
 // ==/UserScript==
 
 (function() {
-    'use strict'
+    'use strict';
 
     console.log('%c[Mod Board] mod loader is up and at em...', 'color: green');
 
-    // Dynamically load your ui.js
+    // Load ui.js first
     const uiScript = document.createElement('script');
     uiScript.src = 'https://whitewingedasmodeus.github.io/Mod-board/ui.js';
-    uiScript.onload = () => console.log('%c[Mod Board] woken up!', 'color: green');
-    uiScript.onerror = () => console.error('%c[Mod Board] slept in... smh', 'color: red');
+    uiScript.onload = () => {
+        console.log('%c[Mod Board] UI loaded, now loading modules...', 'color: green');
+
+        // Load modules after UI is ready
+        const modules = ['rocket', 'drag', 'godmode']; // Add more module names here
+        modules.forEach(name => {
+            const modScript = document.createElement('script');
+            modScript.src = `https://whitewingedasmodeus.github.io/Mod-board/modules/${name}.js`;
+            document.head.appendChild(modScript);
+        });
+    };
+    uiScript.onerror = () => console.error('%c[Mod Board] UI failed to load', 'color: red');
 
     document.head.appendChild(uiScript);
 })();
