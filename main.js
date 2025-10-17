ccdf75aa7c4e2aac9418ad12cffd3b5ecee9860d
skyelar <skyelarkessner@gmail.com>
2025-10-17 17:49:24 -0400

    main.js

--- main.js
+++ main.js
@@ -12,11 +12,11 @@
 
     console.log('%c[Mod Board] mod loader is up and at em...', 'color: green');
 
-    // Dynamically load your UI.js
+    // Dynamically load your ui.js
     const uiScript = document.createElement('script');
-    uiScript.src = 'https://whitewingedasmodeus.github.io/Mod-board/UI.js';
+    uiScript.src = 'https://whitewingedasmodeus.github.io/Mod-board/ui.js';
     uiScript.onload = () => console.log('%c[Mod Board] woken up!', 'color: green');
-    uiScript.onerror = () => console.error('%c[Mod Board] slepped in', 'color: red');
+    uiScript.onerror = () => console.error('%c[Mod Board] slept in... smh', 'color: red');
 
     document.head.appendChild(uiScript);
 })();
