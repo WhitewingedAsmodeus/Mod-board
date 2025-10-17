66ad6a3db0b20a6218fa033a0ce87f7aec5ecf6b
skyelar <skyelarkessner@gmail.com>
2025-10-17 17:42:52 -0400

    main.js
    STFU

--- main.js
+++ main.js
@@ -8,12 +8,15 @@
 // ==/UserScript==
 
 (function() {
-  'use strict'
+    'use strict'
 
-  console.log('%c[Mod Board] Loaded base loader', 'color: lime')
+    console.log('%c[Mod Board] mod loader is up and at em...', 'color: green');
 
-  // Load your existing UI script
-  const uiScript = document.createElement('script')
-  uiScript.src = 'https://WhitewingedAsmodeus.github.io/Mod-board/ui.js'
-  document.head.appendChild(uiScript)
-})()
+    // Dynamically load your UI.js
+    const uiScript = document.createElement('script');
+    uiScript.src = 'https://whitewingedasmodeus.github.io/Mod-board/UI.js';
+    uiScript.onload = () => console.log('%c[Mod Board] woken up!', 'color: green');
+    uiScript.onerror = () => console.error('%c[Mod Board] slepped in', 'color: red');
+
+    document.head.appendChild(uiScript);
+})();
