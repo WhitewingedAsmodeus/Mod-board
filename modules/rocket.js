4557a22c1824642726662282a5cff49b1386a1f6
skyelar <skyelarkessner@gmail.com>
2025-11-16 16:57:19 -0500

    Delete modules directory

--- modules/rocket.js
+++
@@ -1,6 +0,0 @@
-const rocketBtn = ModBoard.createButton("Rocket");
-
-rocketBtn.onclick = () => {
-    const player = ig.game.O4269;
-    if (player && typeof player.vel !== 'undefined') player.vel.y = -1100;
-};
